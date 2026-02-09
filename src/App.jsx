d64427f6f76c658eb930a7e5aa17fa5810f0e1f6
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-09 20:51:29 +0500

    todo basic application is now complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { TodoProvider } from './contexts/TodoContext'
 import { useEffect } from 'react'
 import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
@@ -50,6 +51,14 @@ function App() {
             </div>
             <div className="flex flex-wrap gap-y-3">
                 {/*Loop and Add TodoItem here */}
+                {todos.map(todo => (
+                  <div 
+                  key={todo.id}
+                  className='w-full'
+                  >
+                    <TodoItem todo={todo} />
+                  </div>
+                ))}
             </div>
         </div>
       </div>
