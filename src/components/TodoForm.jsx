bfa61a90cef8833ac6f75fe49f92a6ca27353e32
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-09 18:22:07 +0500

    Create functionality on what happen with form and its inputs when you add a new todo

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTodo } from '../contexts/TodoContext';
 
 function TodoForm() {
-    
+    const [todoMsg, setTodoMsg] = useState("")
+    const {addTodo} = useTodo()
+    const submit = (e) => {
+        e.preventDefault
+        addTodo({todoMsg, completed: false})
+        setTodoMsg("")
+    }
 
     return (
-        <form  className="flex">
+        <form onSubmit={submit}  className="flex">
             <input
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={todoMsg}
+                onChange={(e) => setTodoMsg(e.target.value)}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
