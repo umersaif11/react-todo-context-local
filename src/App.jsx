a04e16ed371ae79b2f3d48713368ab4d944e0208
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-09 14:05:24 +0500

    Create functionality of addTodo which set new todos array by incorporating the new todo

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ import { TodoProvider } from './contexts/TodoContext'
 
 function App() {
   const [todos, setTodos] = useState([])
+  const addTodo = (todo) => {
+    setTodos((prevTodoArray) => [...prevTodoArray, {
+      id: Date.now(),
+      ...todo
+    }])
+  }
 
   return (
-    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo}}>
+    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
