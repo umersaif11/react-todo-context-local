7116a236f182c85b468416290535cdc7d75da04e
Muhammad Umar <108296918+umersaif11@users.noreply.github.com>
2026-02-09 20:29:50 +0500

    Make the identation of the code equal

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -16,7 +16,9 @@ function TodoItem({ todo }) {
 
     return (
         <div
-            className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
+            className={`flex border border-black/10 rounded-lg 
+                px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 
+                duration-300  text-black ${
                 todo.completed ? "bg-[#c6e9a7]" : "bg-[#ccbed7]"
             }`}
         >
@@ -28,8 +30,10 @@ function TodoItem({ todo }) {
             />
             <input
                 type="text"
-                className={`border outline-none w-full bg-transparent rounded-lg ${
-                    isTodoEditable ? "border-black/10 px-2" : "border-transparent"
+                className={`border outline-none w-full 
+                    bg-transparent rounded-lg ${
+                    isTodoEditable ? "border-black/10 px-2" : 
+                    "border-transparent"
                 } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
@@ -37,7 +41,10 @@ function TodoItem({ todo }) {
             />
             {/* Edit, Save Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
+                className="inline-flex w-8 h-8 rounded-lg 
+                text-sm border border-black/10 justify-center 
+                items-center bg-gray-50 hover:bg-gray-100 
+                shrink-0 disabled:opacity-50"
                 onClick={() => {
                     if (todo.completed) return;
 
@@ -51,7 +58,10 @@ function TodoItem({ todo }) {
             </button>
             {/* Delete Todo Button */}
             <button
-                className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
+                className="inline-flex w-8 h-8 rounded-lg 
+                text-sm border border-black/10 justify-center 
+                items-center bg-gray-50 hover:bg-gray-100 
+                shrink-0"
                 onClick={() => deleteTodo(todo.id)}
             >
                 ❌
